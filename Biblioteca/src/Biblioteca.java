82ae651b5503058ab36d57d698961e0394259ea3
henryzitos <127354681+henryzitos@users.noreply.github.com>
2023-05-11 08:18:23 -0300

    Update Biblioteca.java

--- Biblioteca/src/Biblioteca.java
+++ Biblioteca/src/Biblioteca.java
@@ -3,9 +3,46 @@ import java.util.List;
 
 public class Biblioteca {
     private String NomeBiblioteca;
-    private String Dono;
+    private Dono dono;
     private List<Livros> Livros = new ArrayList<>();
     private List<Funcionarios> Funcionarios = new ArrayList<>();
+
+    public Biblioteca(String nomeBiblioteca, Dono dono) {
+        NomeBiblioteca = nomeBiblioteca;
+        this.dono = dono;
+    }
+
+    public String getNomeBiblioteca() {
+        return NomeBiblioteca;
+    }
+
+    public void setNomeBiblioteca(String nomeBiblioteca) {
+        NomeBiblioteca = nomeBiblioteca;
+    }
+
+    public Dono getDono() {
+        return dono;
+    }
+
+    public void setDono(Dono dono) {
+        this.dono = dono;
+    }
+
+    public List<Livros> getLivros() {
+        return Livros;
+    }
+
+    public void setLivros(List<Livros> livros) {
+        Livros = livros;
+    }
+
+    public List<Funcionarios> getFuncionarios() {
+        return Funcionarios;
+    }
+
+    public void setFuncionarios(List<Funcionarios> funcionarios) {
+        Funcionarios = funcionarios;
+    }
 
     public static void mostrarLivros(){
 
